2563cd53cc477e04582c9ba1599b7c99b0274b25
Santhosh Palanisamy <58657228+santhoshsnperode@users.noreply.github.com>
2021-08-23 17:00:08 +0530

    Add files via upload

---
+++ lib/src/helpers/constants.dart
@@ -0,0 +1,5 @@
+class Constant {
+  static const String newsApiKey = '97e6f9d210934fd58b3f5652c40ec899';
+  static const String baseUrl = 'https://newsapi.org/v2/';
+  static const String topHeadLine = '/top-headlines';
+}
